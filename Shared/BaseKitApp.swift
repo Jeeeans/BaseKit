c7dbb7a975decb602cb110dc10097402f7b3365d
sjchin <sjchin@mustit.co.kr>
2021-06-21 09:55:48 +0900

    Initial Commit

---
+++ Shared/BaseKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseKitApp.swift
+//  Shared
+//
+//  Created by Sungjun Chin on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BaseKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
