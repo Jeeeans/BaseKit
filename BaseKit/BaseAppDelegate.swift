910d09499639d08e229acaeaa1961bffc8f75c9a
sjchin <sjchin@mustit.co.kr>
2021-07-08 10:37:01 +0900

    project setting changed

---
+++ BaseKit/BaseAppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  BaseAppDelegate.swift
+//  BaseKit
+//
+//  Created by Sungjun Chin on 2021/07/08.
+//
+
+import Foundation
+import UIKit
+
+open class BaseAppDelegate: UIResponder, UIApplicationDelegate {
+    open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
---
+++ BaseKit/BaseKit.h
@@ -0,0 +1,18 @@
+//
+//  BaseKit.h
+//  BaseKit
+//
+//  Created by Sungjun Chin on 2021/07/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseKit.
+FOUNDATION_EXPORT double BaseKitVersionNumber;
+
+//! Project version string for BaseKit.
+FOUNDATION_EXPORT const unsigned char BaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseKit/PublicHeader.h>
+
+
